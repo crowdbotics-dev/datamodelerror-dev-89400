ca00915f58de99d0afe9e95a40dad2fc10465fe8
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-07 07:01:08 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Mithilesh(models.Model):
+    'Generated Model'
+    studio = models.BigIntegerField()
+    dataModel = models.BooleanField(null=True,blank=True,)
